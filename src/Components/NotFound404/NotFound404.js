e46e4eff765c764ef26ce188cb544522d2681e2f
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-04-04 21:26:25 +0600

    page not found updated

--- src/Components/NotFound404/NotFound404.js
+++ src/Components/NotFound404/NotFound404.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import notfound from '../Images/notfound.jpg'
 const NotFound404 = () => {
     return (
         <div>
-            <h2>page not found</h2>
+            <div className="container">
+                <div className="row"><img src={ notfound } alt="notfound" /></div>
+            </div>
         </div>
     );
 };
